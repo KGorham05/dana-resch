4e239299a381eb4b87bc51a0d8ddff7fa6fafcba
Kgor <kevin.gorham@gmail.com>
2019-10-22 20:34:32 -0700

    working hover effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
 console.log('Hello World!')
+
+$("#name").on('click', function (e) {
+    e.preventDefault();
+    $(".nav").hasClass("invisible") ? $(".nav").removeClass("invisible") : $(".nav").addClass("invisible")
+
+})
+$("#name").hover(function (e) {
+    e.preventDefault();
+    $(".nav").removeClass("invisible")
+})
