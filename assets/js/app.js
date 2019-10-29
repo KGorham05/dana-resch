8ccba48deb9dd236a28bc56066bd107640812773
Kgor <kevin.gorham@gmail.com>
2019-10-29 13:50:24 -0700

    added images, working nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,18 @@
-console.log('Hello World!')
+// console.log('Hello World!')
 
-$("#name").on('click', function (e) {
-    e.preventDefault();
-    $(".nav").hasClass("invisible") ? $(".nav").removeClass("invisible") : $(".nav").addClass("invisible")
+// $("#name").on('click', function (e) {
+//     e.preventDefault();
+//     $(".nav").hasClass("invisible") ? $(".nav").removeClass("invisible") : $(".nav").addClass("invisible")
+//     console.log('Clicked!')
+// })
+// $("#name").hover(function (e) {
+//     e.preventDefault();
+//     $(".nav").removeClass("invisible")
+// })
 
-})
-$("#name").hover(function (e) {
-    e.preventDefault();
-    $(".nav").removeClass("invisible")
-})
+var distance = $('#navbar').offset().top,
+    $window = $(window);
+
+$window.scroll(function () {
+    $('#navbar').toggleClass('fixed-top', $window.scrollTop() >= distance)
+});
